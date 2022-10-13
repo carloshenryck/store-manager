29a4f10200bb70b89f4fc63159f9346e17243a84
Henryck <ryck302@gmail.com>
2022-10-13 00:18:22 -0300

    requisito 01

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,21 @@
+const services = require('../services');
+const errorMap = require('../utils/errorMap');
+
+const getAllProducts = async (_req, res) => {
+  const { message } = await services.productService.findAll();
+  res.status(200).json(message);
+};
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await services.productService.findById(id);
+
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
+
+  res.status(200).json(message);
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
---
+++ src/services/product.service.js
@@ -0,0 +1,21 @@
+const models = require('../models');
+const { validateId } = require('./validations/valuesValidations');
+
+const findAll = async () => {
+  const products = await models.productModel.findAll();
+  return { type: null, message: products };
+};
+
+const findById = async (productId) => {
+  const error = validateId(productId);
+  if (error.type) return error;
+
+  const product = await models.productModel.findById(productId);
+  if (!product) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+  return { type: null, message: product };
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
